8a6fa8f39fd0f5c7da3c0ee1bc464b4a0af2983f
Sudhanshu Bhagwat <sudhanshu_satishbhagwat@trimble.com>
2020-04-17 22:58:05 +0530

    Added dotenv

--- src/db.js
+++ src/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 const db = mongoose.connect(`mongodb://${process.env.mdbuser}:${process.env.mdbpass}@ds211648.mlab.com:11648/corona`, 
         { useNewUrlParser: true , useUnifiedTopology: true} ,(err) => {
     if(err) throw (err);
